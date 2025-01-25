830a6c1c4944f2e6d4154e78628e9c3cb1721a20
Parth-bathe0142 <parth.bathe0142@gmail.com>
2025-01-25 13:55:21 +0530

    dead commit

--- src/Scene/Scene.ts
+++ src/Scene/Scene.ts
@@ -5,26 +5,119 @@ import { Coord } from "../models/core/misc.js"
 import { Time } from "../models/core/Time.js"
 import { PixelMap } from "./PixelMap.js"
 
-export interface SceneJSON {
-    objects: { [key: string]: GameObjectConfig }
+interface SceneConfig {
+    mapConfig?: {
+        name: string
+        spritesheetSize: [number, number]
+    }
+    playerPos?: Coord
 }
 
-export interface Scene {
-    objects: GameObject[]
+export class Scene {
+
+    ctx: CanvasRenderingContext2D
+    canvas: HTMLCanvasElement
+
+    objects: GameObject[] = []
     interactables: Map<Coord, GameObject>
     player: Player
     map: PixelMap
     time: Time
     camera: Camera
+    keyInput: KeyInput
+    pathFinder: PathFinder
 
-    ispaused: boolean
-    
-    load(): Promise<void>
-    init(): void
-    update(): void
-    render(): void
-    pause(): void
-    play(): void
-    togglePause(): boolean
-    isSpaceValid(coord: Coord): boolean
+    isPaused: boolean
+
+    constructor() {
+        this.canvas = document.getElementById("game-canvas")! as HTMLCanvasElement
+        this.ctx = this.canvas.getContext("2d")!
+        this.objects = []
+        this.interactables = new Map<Coord, GameObject>
+        this.player = new Player({
+            gridPos: { x: 5, y: 7 },
+            name: "player",
+            scene: this,
+            spriteConfig: {
+                src: "assets/spritesheets/character.png",
+                currentAnim: "idle-down"
+            }
+        })
+        this.map = new PixelMap("example_map", 6, 8)
+        this.time = new Time(48)
+        this.camera = new Camera({
+            object: this.player
+        })
+        this.keyInput = new KeyInput({ puppet: this.player })
+        this.pathFinder = new PathFinder()
+
+        this.isPaused = false
+
+        this.load()
+    }
+
+    async load() {
+        await this.map.load()
+        const bmap = PathFinder.mapFromPixelMap(this.map)
+        this.pathFinder.setMap(bmap, this.map.height, this.map.width)
+        this.init()
+    }
+
+    update = () => {
+        this.player.update()
+        this.objects.forEach(object => object.update())
+    }
+
+    render = () => {
+        const gameState = {
+            camera: this.camera,
+            time: this.time
+        }
+
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+        this.map.drawLayer(this.ctx, gameState, "Water");
+        this.map.drawLayer(this.ctx, gameState, "Ground");
+        this.map.drawLayer(this.ctx, gameState, "main");
+        for (const object of this.objects) {
+            object.sprite.draw(this.ctx, gameState)
+        }
+        this.player.sprite.draw(this.ctx, gameState)
+        this.map.drawLayer(this.ctx, gameState, "Roof");
+    }
+
+    init() {
+        const { pause, play } = this.time.runLoop(this.update, this.render)!
+        this.pause = pause
+        this.play = play
+    }
+
+    pause?: () => void
+    play?: () => void
+
+    togglePause() {
+        if (this.isPaused) {
+            this.play?.()
+            this.isPaused = false
+        } else {
+            this.pause?.()
+            this.isPaused = true
+        }
+    }
+
+    isSpaceValid(_coord: Coord) {
+        const coord = utils.coordToString(_coord)
+        
+        if (this.map.layers["Ground"].tiles.has(coord) && !this.map.layers["main"].tiles.has(coord)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
 }
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,17 @@
-import { MovableObjectGrid } from "./models/core/MovableObject.js"
-import { Time } from "./models/core/Time.js"
-import { Camera } from "./models/core/Camera.js"
-import { PixelMap } from "./Scene/PixelMap.js"
-import { KeyInput } from "./models/core/KeyInput.js"
-import { Player } from "./models/characters/Player.js"
+import { Scene } from ""
+const scene = new Scene({
+    mapConfig: {
+        name: "example_map",
+        spritesheetSize: [6, 8]
+    },
+    playerPos: { x: 4, y: 4 }
+});
+setTimeout(async() => {
+    await scene.time.delay(5)
+    await scene.player.startWalkTo({ x: 11, y: 5 })
+    await scene.time.delay(5)
+    await scene.player.startWalkTo({ x: 10, y: 18 })
+    await scene.time.delay(5)
+    await scene.player.startWalkTo({ x: 8, y: 7 })
+}, 200);
 
-// HTML Element that supports drawing custom images
-const canvas = document.querySelector("canvas#game-canvas") as HTMLCanvasElement
-
-// a "pen" associated with a canvas that is used for drawing to that canvas
-const ctx = canvas.getContext("2d")!
-
-// example of creating an object using configs, skipping some optional parameters
-const object = new Player({
-    gridPos: { x: 12, y: 7 },
-    name: "player",
-    spriteConfig: {
-        src: "assets/spritesheets/character.png",
-        currentAnim: "idle-down"
-    }
-})
-
-const object2 = new MovableObjectGrid({
-    gridPos: { x: 10, y: 5 },
-    spriteConfig: {
-        src: "assets/spritesheets/character.png",
-        cropSize: { width: 48, height: 48 },
-        drawSize: { width: 48, height: 48 },
-        drawOffset: { x: 0, y: -4 },
-        isAnimated: false
-    }
-})
-
-const newMap = new PixelMap("example_map", 6, 8)
-
-const camera = new Camera({ object: object })
-function update() {
-    object.update()
-    object2.update()
-}
-function render() {
-    const gameState = { camera, time }
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    newMap.drayLayer(ctx, gameState, "Water")
-    newMap.drayLayer(ctx, gameState, "Ground")
-    newMap.drayLayer(ctx, gameState, "main")
-    object.sprite.draw(ctx, gameState)
-    object2.sprite.draw(ctx, gameState)
-    newMap.drayLayer(ctx, gameState, "Roof")
-}
-
-const keyInput = new KeyInput({ puppet: object })
-
-const time = new Time(48)
-const { pause, play } = time.runLoop(update, render)!
