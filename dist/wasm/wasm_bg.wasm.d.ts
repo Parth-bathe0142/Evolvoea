6f6561ff31994bdeab98a155db55a8bb4511d427
Parth-bathe0142 <parth.bathe0142@gmail.com>
2025-03-31 13:01:40 +0530

    .

---
+++ dist/wasm/wasm_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const set_map: (a: number, b: number, c: number, d: number) => number;
+export const find_path: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
